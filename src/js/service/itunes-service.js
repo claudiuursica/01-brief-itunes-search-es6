d7059f54d76b57f359d23f0759ecdac6eebcc717
claudiuursica <claudiu.ursica@gmail.com>
2017-12-06 16:32:48 +0000

    Update itunes-service.js

--- src/js/service/itunes-service.js
+++ src/js/service/itunes-service.js
@@ -2,14 +2,25 @@
 
 import makeRequest from './api-xhr-request';
 import {URLBuilder} from "./url-builder";
+import fetchJSONP from './api-jsonp-request';
 
 /**
  * @param {string} query
  * @return {Promise}
  */
-export default function search (query) {
-  const url = URLBuilder.buildURL(query);
+export function search(query) {
+	const url = URLBuilder.buildURL(query);
+	
+	return makeRequest({url: url})
+		.then(data => data = JSON.parse(data));
+}
 
-  return makeRequest({ url: url })
-    .then(data => data = JSON.parse(data))
+/**
+ * @param {string} query
+ * @return {Promise}
+ */
+export function searchJSONP(query) {
+	const url = URLBuilder.buildURL(query);
+	
+	return fetchJSONP(url).then(response => response.json());
 }
