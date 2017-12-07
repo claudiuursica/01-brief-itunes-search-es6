654f279c635d3138c1f507d6edb189d3e38057bc
claudiuursica <claudiu.ursica@gmail.com>
2017-12-07 16:38:20 +0000

    Update and rename url-builder-spec.js to build-url-spec.js

---
+++ src/js/service/build-url-spec.js
@@ -0,0 +1,17 @@
+import { BASE_API } from 'service/api';
+import { buildURL } from 'service/build-url';
+
+describe('Function: buildURL', () => {
+
+  it('should return base urls when arguments are invalid', () => {
+    expect(buildURL()).toBe(BASE_API);
+  });
+
+  it('should return the search urls when arguments are valid', () => {
+
+    const searchOptions = 'jack johnson';
+    const expectedResult = 'https://itunes.apple.com/search?term=jack+johnson&limit=10';
+
+    expect(buildURL(searchOptions)).toBe(expectedResult);
+  });
+});
