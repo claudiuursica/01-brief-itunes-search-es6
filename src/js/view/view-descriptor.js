0f4e4de85781efcebf56ec9c9526452b81a366a0
claudiuursica <claudiu.ursica@gmail.com>
2018-01-10 16:44:09 +0000

    Create view-descriptor.js

---
+++ src/js/view/view-descriptor.js
@@ -0,0 +1,20 @@
+'use strict';
+
+export const HEADER = [
+	{
+		label    : '',
+		className: ''
+	},
+	{
+		label    : 'SONG',
+		className: 'item'
+	},
+	{
+		label    : 'ARTIST',
+		className: 'item'
+	},
+	{
+		label    : 'ALBUM',
+		className: 'item'
+	}
+];
