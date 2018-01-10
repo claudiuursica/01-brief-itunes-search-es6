0266c7dc394ae1178b8e0ecd85163f21398aca2f
claudiuursica <claudiu.ursica@gmail.com>
2018-01-10 16:43:19 +0000

    Update view-factory.js

--- src/js/view/view-factory.js
+++ src/js/view/view-factory.js
@@ -1,9 +1,49 @@
 'use strict';
 
+import { HEADER } from "view/view-descriptor";
+
+/**
+ * @param {Array.<Object>} data
+ */
+export function renderData(data)
+{
+	const listElement = createList();
+	listElement.appendChild(createListHeader(HEADER));
+	listElement.appendChild(createListBody(data));
+	return listElement;
+}
+
+/**
+ * @returns {HTMLElement}
+ */
+export function createList() {
+	return document.createElement('ul');
+}
+
+/**
+ * @param {Object} data
+ * @returns {HTMLElement}
+ */
+export function createListHeader(data) {
+	const listHeaderElement = document.createElement('li');
+	listHeaderElement.appendChild(createListHeaderElements(data));
+	return listHeaderElement;
+}
+
+/**
+ * @param {Object} data
+ * @returns {DocumentFragment}
+ */
+export function createListBody(data) {
+	let fragment = document.createDocumentFragment();
+	data.forEach((item) => fragment.appendChild(createListItemElement(item, 'item')));
+	return fragment;
+}
+
 /**
  * @param {string} text
  * @param {string} className
- * @returns {Element}
+ * @returns {HTMLElement}
  */
 export function createSpanElement (text, className = '') {
 	const spanElement = document.createElement('span');
@@ -16,9 +56,20 @@ export function createSpanElement (text, className = '') {
 }
 
 /**
+ * @param {Array.<Object>} config
+ * @returns {DocumentFragment}
+ */
+export function createListHeaderElements(config)
+{
+	let fragment = document.createDocumentFragment();
+	config.forEach((item) => fragment.appendChild(createSpanElement(item.label, item.className)));
+	return fragment;
+}
+
+/**
  * @param {string} text
  * @param {string} className
- * @returns {Element}
+ * @returns {HTMLElement}
  */
 export function createListItemElement (options, className = '') {
 	const liElement = document.createElement('li');
