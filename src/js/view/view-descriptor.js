3a94ca43449f7752eb13bdeb3684afb7a16c8d2d
claudiuursica <claudiu.ursica@gmail.com>
2018-01-15 14:52:18 +0000

    Update view-descriptor.js

--- src/js/view/view-descriptor.js
+++ src/js/view/view-descriptor.js
@@ -18,3 +18,36 @@ export const HEADER = [
 		className: 'item'
 	}
 ];
+
+export const BODY = [
+	{
+		index         : 1,
+		trackName     : 'trackName1',
+		artistName    : 'artistName1',
+		collectionName: 'collectionName1'
+	},
+	{
+		index         : 2,
+		trackName     : 'trackName2',
+		artistName    : 'artistName2',
+		collectionName: 'collectionName2'
+	},
+	{
+		index         : 3,
+		trackName     : 'trackName3',
+		artistName    : 'artistName3',
+		collectionName: 'collectionName3'
+	},
+	{
+		index         : 4,
+		trackName     : 'trackName4',
+		artistName    : 'artistName4',
+		collectionName: 'collectionName4'
+	},
+	{
+		index         : 5,
+		trackName     : 'trackName5',
+		artistName    : 'artistName5',
+		collectionName: 'collectionName5'
+	}
+];
