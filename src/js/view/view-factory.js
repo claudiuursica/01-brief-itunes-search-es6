e66d921f069ef68366c1a4c11d94b22735937509
claudiuursica <claudiu.ursica@gmail.com>
2018-02-22 16:51:28 +0000

    Update view-factory.js

--- src/js/view/view-factory.js
+++ src/js/view/view-factory.js
@@ -1,11 +1,12 @@
 'use strict';
 
-import { HEADER } from "view/view-descriptor";
+import {HEADER} from "view/view-descriptor";
+import {truncate21, truncate31} from "utils/utils";
 
 /**
  * @param {Array.<Object>} data
  */
-export function renderData(data)
+export function renderItems(data)
 {
 	const listElement = createList();
 	listElement.appendChild(createListHeader(HEADER));
@@ -14,9 +15,21 @@ export function renderData(data)
 }
 
 /**
+ * @param {string} message
+ * @param {string} className
+ */
+export function renderMessage(message, className = '')
+{
+	const divElement = createDivElement(className);
+	divElement.appendChild(createSpanElement(message));
+	return divElement;
+}
+
+/**
  * @returns {HTMLElement}
  */
-export function createList() {
+export function createList()
+{
 	return document.createElement('ul');
 }
 
@@ -24,7 +37,8 @@ export function createList() {
  * @param {Object} data
  * @returns {HTMLElement}
  */
-export function createListHeader(data) {
+export function createListHeader(data)
+{
 	const listHeaderElement = document.createElement('li');
 	listHeaderElement.appendChild(createListHeaderElements(data));
 	return listHeaderElement;
@@ -34,9 +48,10 @@ export function createListHeader(data) {
  * @param {Object} data
  * @returns {DocumentFragment}
  */
-export function createListBody(data) {
+export function createListBody(data)
+{
 	let fragment = document.createDocumentFragment();
-	data.forEach((item) => fragment.appendChild(createListItemElement(item, 'item')));
+	data.forEach(item => fragment.appendChild(createMainListItemElement(item, 'item')));
 	return fragment;
 }
 
@@ -45,14 +60,31 @@ export function createListBody(data) {
  * @param {string} className
  * @returns {HTMLElement}
  */
-export function createSpanElement (text, className = '') {
+export function createSpanElement(text, className = '')
+{
 	const spanElement = document.createElement('span');
 	spanElement.innerHTML = text;
-	if (className) {
+	if (className)
+	{
 		spanElement.classList.add(className);
 	}
-	
+
 	return spanElement;
+}
+
+/**
+ * @param {string} className
+ * @returns {HTMLElement}
+ */
+export function createDivElement(className = '')
+{
+	const divElement = document.createElement('div');
+	if (className)
+	{
+		divElement.classList.add(className);
+	}
+
+	return divElement;
 }
 
 /**
@@ -67,18 +99,86 @@ export function createListHeaderElements(config)
 }
 
 /**
+ * @param {Function} generator
  * @param {string} text
  * @param {string} className
  * @returns {HTMLElement}
  */
-export function createListItemElement (options, className = '') {
+export function createListItemElement(generator, options, className = '')
+{
 	const liElement = document.createElement('li');
-	
-	const content = `<span>${options.index}</span>
-										<span class="${className}">${options.trackName}</span>
-										<span class="${className}">${options.artistName}</span>
-										<span class="${className}">${options.collectionName}</span>`;
-	
-	liElement.innerHTML = content;
+	liElement.innerHTML = generator(options, className);
 	return liElement;
+}
+
+/**
+ * @param {string} text
+ * @param {string} className
+ * @returns {HTMLElement}
+ */
+export function createMainListItemElement(options, className = '')
+{
+	return createListItemElement(createListItemFromOptions, options, className);
+}
+
+/**
+ * @param {string} text
+ * @param {string} className
+ * @returns {HTMLElement}
+ */
+export function createFavListItemElement(options, className = '')
+{
+	return createListItemElement(createFavListItemFromOptions, options, className);
+}
+
+/**
+ * @param {string} text
+ * @param {string} className
+ * @returns {string}
+ */
+function createListItemFromOptions(options, className = '')
+{
+	return `<span title="${options.trackName}" class="${className}">${truncate31(options.trackName)}</span>
+					 <span class="${className}">${truncate31(options.artistName)}</span>
+					 <span class="${className}">${truncate31(options.collectionName)}</span>
+					 <button id="${options.trackId}">+</button>`;
+}
+
+/**
+ * @param {HTMLElement} element
+ */
+export function removeAllChildren(element)
+{
+	if (element) {
+		while (element.firstChild) {
+			element.removeChild(element.firstChild);
+		}
+	}
+}
+
+/**
+ * @param {HTMLElement|Node} element
+ * @param {Object} data
+ */
+export function append(element, child)
+{
+	if (element && child) {
+		element.appendChild(child);
+	}
+}
+
+/**
+ * @param {string} text
+ * @param {string} className
+ * @returns {string}
+ */
+function createFavListItemFromOptions(options, className = '')
+{
+	return `<div class="favourite">
+						<img src="${options.artworkUrl30}"/>
+				  	<div>
+					    <span class="${className}">${truncate21(options.trackName)}</span>
+							<span>${truncate21(options.artistName)}</span>					
+						</div>
+					</div>`;
 }
