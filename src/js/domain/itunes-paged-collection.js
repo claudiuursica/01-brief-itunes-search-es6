7e1a798bbda8a69fcbc4982c1f6023fcc2de4158
claudiuursica <claudiu.ursica@gmail.com>
2018-02-22 16:55:33 +0000

    Update itunes-paged-collection.js

--- src/js/domain/itunes-paged-collection.js
+++ src/js/domain/itunes-paged-collection.js
@@ -1,7 +1,7 @@
 'use strict';
 
 export class ITunesPagedCollection {
-	
+
 	constructor() {
 		this.itemsList = [];
 		this.pageItemsList = [];
@@ -9,43 +9,49 @@ export class ITunesPagedCollection {
 		this.numberOfItemsPerPage = 10;
 		this.numberOfPages = 0;
 	}
-	
+
 	/**
 	 * @param {Array.<Object>} items
 	 */
-	setItems(items) {
+	setItems(items, numberOfItemsPerPage) {
+
 		this.itemsList = items || [];
+		this.numberOfItemsPerPage = numberOfItemsPerPage;
 		this.numberOfPages = this.getNumberOfPages();
-		this.loadCurrentPageInPageList();
+		this.firstPage();
 	}
-	
+
 	getNumberOfPages() {
 		return Math.ceil(this.itemsList.length / this.numberOfItemsPerPage);
 	}
-	
+
 	nextPage() {
 		this.currentPage += 1;
 		this.loadCurrentPageInPageList();
 	}
-	
+
 	previousPage() {
 		this.currentPage -= 1;
 		this.loadCurrentPageInPageList();
 	}
-	
+
 	firstPage() {
 		this.currentPage = 1;
 		this.loadCurrentPageInPageList();
 	}
-	
+
 	lastPage() {
 		this.currentPage = this.numberOfPages;
 		this.loadCurrentPageInPageList();
 	}
-	
+
 	loadCurrentPageInPageList() {
 		const begin = ((this.currentPage - 1) * this.numberOfItemsPerPage);
 		const end = begin + this.numberOfItemsPerPage;
 		this.pageItemsList = this.itemsList.slice(begin, end);
+	}
+
+	find(fun) {
+		return this.itemsList.find(fun);
 	}
 }
